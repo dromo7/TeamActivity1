70f58f4aed21df64b688b428c4a5e72022e931d4
dromo7 <dromo7@horizon.csueastbay.edu>
2025-09-23 12:53:12 -0700

    changes

--- Menu.cpp
+++ Menu.cpp
@@ -2,3 +2,20 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
+using namespace std;
+
+int main () {
+    int choice;
+    int addCorrect = 0;
+    int addIncorrect = 0;
+    int subCorrect = 0;
+    int subIncorrect = 0;
+    int multCorrect = 0;
+    int multIncorrect = 0;
+    int divCorrect = 0;
+    int divIncorrect = 0;
+    int num1;
+    int num2;
+    int answer;
+    srand(time(0));
+}
