72c94faf6959189ddc08ce07a469c708fccc73e6
ibaltazarespinosa-lab <ibaltazarespinosa@horizon.csueastbay.edu>
2025-09-23 12:54:24 -0700

    menu

--- Menu.cpp
+++ Menu.cpp
@@ -19,3 +19,14 @@ int main () {
     int answer;
     srand(time(0));
 }
+while (true){
+    cout << "Welcome to the math quiz!" << endl;
+    cout << "Please select the type of problems you want to practice:" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Statistics" << endl;
+    cout << "q. Quit" << endl;
+    cout << "Enter your choice (1-4): ";
+}
