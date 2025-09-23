250aa14f85a4d6c25e4e3e2e3e26dd6562dccef0
dromo7 <dromo7@horizon.csueastbay.edu>
2025-09-23 12:23:35 -0700

    menu changes

--- Menu.cpp
+++ Menu.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main() {
     int choice;
+    int d_count = 0;
+    int d_icount = 0;
+    string user_input;
+    int numerator;
+    int denomenator;
+    int user_answer;
+    int final_answer;
     cout << "Welcome to the Math Quiz!" << endl;
     cout << "Please select the type of problems you want to practice:" << endl;
     cout << "1. Addition" << endl;
@@ -56,11 +63,39 @@ int main() {
         cout << "Do you want to try another problem? ('y' for Yes and '-999/a' for No): ";
         cin >> response;
         break;}
-    
+            case 3: {
+
             }
+            case 4: {
+                while (user_input != "-999/a"){
+                srand(time(0));
+                numerator = rand() % 100 + 2;
+                denomenator = rand() % 50 + 2;
+                    while (numerator % denomenator > 0) {
+                        numerator = rand() % 100 + 2;
+                        denomenator = rand() % 50 + 2;}
+                        final_answer = numerator / denomenator;
+                        std::cout << "What is " << numerator << " / " << denomenator << "? ";
+                        cin >> user_answer;
+                            if (user_answer == final_answer){
+                                std::cout << "Correct" << endl;
+                                d_count++;
+                            }
+                            else {
+                                std::cout << "Incorrect. The answer is " << final_answer << "." << endl;
+                                d_icount++;
+                            }
+                            cout << "Correct: " << d_count << endl;
+                            cout << "Incorrect: " << d_icount << endl;
+                            cout << "Do you want to try another problem? ('y' for Yes and '-999/a for no): ";
+                            cin >> user_input;
+                            }
+                break;}
+            case 5: {
+                
         }
 
         }
 
-    return 0;
+    
 
